b0c0a7b5a76a236f630ef862cba9f9a9d91dabbf
kulrawee.n <56444113+kulraween@users.noreply.github.com>
2021-03-14 11:38:18 +0100

    Create Exercise 5.2.py

---
+++ Exercise 5.2.py
@@ -0,0 +1,18 @@
+list = []
+
+while True:
+    num = input("Enter a number: ")
+    if num == "done": 
+        break
+    else:
+    	try:
+			num = int(num)
+			list.append(num)
+    	except:
+        	print("Invalid input")
+            
+largest = max(list)
+smallest = min(list)
+   
+print("Maximum is", largest)
+print("Minimum is", smallest)
